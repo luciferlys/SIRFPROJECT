a3486034809a6575dc5e14ca9658ee44809c0011
Kris Thielemans <k.thielemans@ucl.ac.uk>
2021-08-12 14:40:10 +0100

    skeleton for creating a numpy array

--- test_functionality.py
+++ test_functionality.py
@@ -1,6 +1,9 @@
 import ala
-c= ala.STIRImageData('D:/SIRF/SIRFbuild/INSTALL/share/SIRF-3.1/data/examples/PET/test_image_PM_QP_6.hv') 
-b= ala.PETAcquisitionDataInFile('D:/SIRF/SIRFbuild/INSTALL/share/SIRF-3.1/data/examples/PET/Utahscat600k_ca_seg4.hs')
+SIRFpath='D:/SIRF/SIRFbuild/INSTALL/'
+SIRFpath='/home/sirfuser/devel/install'
+c= ala.STIRImageData(SIRFpath + '/share/SIRF-3.1/data/examples/PET/test_image_PM_QP_6.hv') 
+b= ala.PETAcquisitionDataInFile(SIRFpath + '/share/SIRF-3.1/data/examples/PET/Utahscat600k_ca_seg4.hs')
 #c.fill(0)
 print(c.dot(c))
+print(c.as_array().shape)
 print(b.dot(b))
