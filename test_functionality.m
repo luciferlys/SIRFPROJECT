86a83220862c1d7a4bb8fa2560f6c2eb645b6f0b
lys <luciferlysss@gmail.com>
2022-01-31 12:07:42 +0000

    [finished] add the test for as_array() the imshow() kept making MATLAB crashing therefore size() was used as back-up plan to prove that it worked well

--- test_functionality.m
+++ test_functionality.m
@@ -5,8 +5,8 @@ addpath '/home/sirfuser/devel/install/matlab/'
 %%
 % Data reading & creation
 stir_data_null = ala.STIRImageData();
-%stir_data_null.initialise([40,5,6], [1,1,1]);
-
+stir_data_null.initialise(40,5,6,1,1,1);
+disp(stir_data_null.get_geom_info_sptr().get_info());
 %%
 stir_data_1 = ala.STIRImageData('/home/sirfuser/devel/install/share/SIRF-3.1/data/examples/PET/test_image_PM_QP_6.hv');
 disp(stir_data_1.get_geom_info_sptr().get_info());
@@ -20,7 +20,10 @@ acq_data_test = sirf.STIR.AcquisitionData('/home/sirfuser/devel/install/share/SI
 %%
 % [Unfinished]save figure(wait for as_array())
 im = ala.STIRImageData('/home/sirfuser/devel/install/share/SIRF-3.1/data/examples/PET/brain/emission.hv');
-
+im_array = im.as_array();
+im_array_one_pic = im_array(5,:,:);
+%im_array_one_pic = reshape(im_array_one_pic,211,211);
+%imshow(im_array_one_pic)
 %% construct a "template" for the acquisition data
 sino_template = ala.PETAcquisitionDataInMemory('PRT-1',1,1);
 
@@ -51,3 +54,9 @@ fprintf('the result of acq_data.norm() is %g.\n', acq_data.norm());
 fprintf('the result of acq_data_test.norm() is %g.\n', acq_data_test.norm());
 %fprintf('the reuslt of acq_data.is_complex() is %d (0 stands for false, 1 stands for true).\n', acq_data.is_complex());
 fprintf('the reuslt of acq_data_test.is_complex() is %d (0 stands for false, 1 stands for true).\n', acq_data_test.is_complex());
+
+%% test as_array()
+disp('the dimensions of stir_data_1 in each dimension is:');
+disp(size(stir_data_1.as_array()));
+disp('the dimensions of stir_data_test in each dimension is:');
+disp(size(stir_data_test.as_array()));
